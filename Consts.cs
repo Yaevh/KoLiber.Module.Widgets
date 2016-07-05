e185a8c6d3343bdfcfcee481f8527cacf432a7ea
T.M <tomasz.piotr.malinowski@gmail.com>
2016-07-06 00:34:32 +0200

    removed magic strings from KoLiber.Widgets.Dotpay

---
+++ Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KoLiber.Module.Widgets {
+    public class Consts {
+
+        public const string DotpayFeatureID = "KoLiber.Widgets.Dotpay";
+        public const string DotpayWidgetName = "DotpayWidget";
+
+    }
+}
